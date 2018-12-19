115d0dd2d0c04ecce7ea33529d04fea893ab7285
Tomasz Klag <tomek.klag@gmail.com>
2018-12-19 08:43:17 +0100

    commit 2

---
+++ src/app/hero.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: Send the message after fatching the heroes.
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: Send the message after fatching the hero.
+    this.messageService.add(`HeroService: fethed hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
